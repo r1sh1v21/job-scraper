fa6e7f2193e8e6d2d3f7cad8c33094d9abd5fe43
Rishi <valee.rishi@protonmail.com>
2023-07-28 16:25:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 def scrape_MBRDNA():
     url = 'https://jobs.lever.co/MBRDNA'  
